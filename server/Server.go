4ffb8a859a71b174203356074febe158d158dd12
OfficialChutney <noav@itu.dk>
2025-11-12 11:09:05 +0100

    Made comments to code

--- server/Server.go
+++ server/Server.go
@@ -15,6 +15,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+// Below we make our predefined identifier
 type STATE int
 
 const (
@@ -23,6 +24,7 @@ const (
 	HELD
 )
 
+// Our global variables. Explained when used...
 var (
 	id      int
 	state   = RELEASED
@@ -36,6 +38,7 @@ var (
 	basePort = 8000
 )
 
+// Max function for lamport clock.
 func max(a, b int32) int32 {
 	if a > b {
 		return a
@@ -43,19 +46,38 @@ func max(a, b int32) int32 {
 	return b
 }
 
+// Our server struct.
 type RicartAgrawalaServer struct {
 	pb.UnimplementedRicartAgrawalaServer
 }
 
+// Receive function as pr. Ricart Agrawala
 func (s *RicartAgrawalaServer) Receive(ctx context.Context, req *pb.Request) (*pb.Reply, error) {
+	//We choose to lock, as our receive checks a lot of our global variables.
 	mu.Lock()
+	//We update our lamport clock.
 	clock = max(clock, req.Timestamp) + 1
 
+	/*We now check whether or not the request from a node should be replied to,
+	or if we want or already are in the CS, and therefor shoudl be queued.
+	* myReqTS is the clock at the time this node called it's Enter method, as the clock may change while this
+	node is waiting to enter the CS.
+	*/
 	for state == HELD || (state == WANTED && myReqTS < req.Timestamp) {
+		/*We make a done channel aswell as a goroutine that checks the context whether we have lost
+		connection to the node. It just waits for the context channel to return, and if so we close our done channel.
+		We do this in a seperate go routine since waiting for the context channel blocks the thread.
+		*/
 		done := make(chan struct{})
 		go func() { <-ctx.Done(); close(done) }()
+
+		//Here we use a Condition variable and wait for a broadcast from our Exit. This releases the lock
+		// and afterwards reacquires it when we receive a broadcast. This esentially also works as our queue,
+		// since the Condition Variables releases the lock in order of which the Wait was called.
 		cond.Wait()
 
+		//Here we just check if we have received a reply from our done channel. If so, the node is dead,
+		//and we therefor release the lock and dont return anything.
 		select {
 		case <-done:
 			mu.Unlock()
@@ -64,6 +86,8 @@ func (s *RicartAgrawalaServer) Receive(ctx context.Context, req *pb.Request) (*p
 		}
 	}
 
+	//If we reach here, it means that the node asking was prioritised over this current node,
+	//or the Exit function has been called. We therefor reply to the node.
 	clock++
 	mu.Unlock()
 	fmt.Printf("I AM REPLYING TO PORT %d", basePort+int(req.SenderId))
@@ -71,6 +95,9 @@ func (s *RicartAgrawalaServer) Receive(ctx context.Context, req *pb.Request) (*p
 }
 
 func Enter() {
+	//Here we lock since we access global variables.
+	//We increase our clock and save the state of our clock to a seperate variable,
+	//To ensure correct order on receives (since the clock can increase concurrently).
 	mu.Lock()
 	clock++
 	myReqTS = clock
@@ -79,26 +106,40 @@ func Enter() {
 	mu.Unlock()
 
 	var wg sync.WaitGroup
+
 	fmt.Println("I AM ASKING ALL PEERS")
+	//We loop through all our peer nodes.
 	for _, addr := range peers {
+		//We set the address to a new variable. This is to ensure each goroutine uses a seperate memory address.
 		addr := addr
+		//We add 1 to our waitgroup
 		wg.Add(1)
+		//We ask each peer in a seperate goroutine, so we can have each thread blocked until reply.
 		go func() {
+			//When reply, substract from waitgroup
 			defer wg.Done()
 
+			//Create context. Give it a max of 15 second timeout for response
 			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			//Cancel when done.
 			defer cancel()
 
+			//Dial the connection.
 			conn, _ := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
+			//Close the connection when done.
 			defer conn.Close()
 
+			//Make new Client.
 			c := pb.NewRicartAgrawalaClient(conn)
 
+			//Here we lock, as we are accessing our global clock. We set a new local variable to our clock,
+			//So we dont lock while waiting for reply.
 			mu.Lock()
 			ts := clock
 			mu.Unlock()
 
+			//We call our receive function. Here the thread is blocked until we receive a reply.
 			_, _ = c.Receive(ctx, &pb.Request{
 				SenderId:  int32(id),
 				Timestamp: ts,
@@ -107,8 +148,11 @@ func Enter() {
 		}()
 	}
 
+	//We wait for our waitgroup to be done. When all goroutines waiting for replies from peers is done,
+	//We can continue execution.
 	wg.Wait()
 
+	//We lock, change our state to held. When we reach here, all our peers have replied.
 	mu.Lock()
 	fmt.Println("STATE IS HELD - IN THE CRITICAL SECTION")
 	state = HELD
@@ -116,14 +160,22 @@ func Enter() {
 }
 
 func Exit() {
+	//We lock, change our clock, set our state to released.
 	mu.Lock()
 	clock++
 	fmt.Println("STATE IS RELEASED")
 	state = RELEASED
 	mu.Unlock()
+	//We broadcast to all peers waiting in queue, that they can reclaim the lock and continue exection.
+	//This will reply to all peers in queue from receive method.
 	cond.Broadcast()
 }
 
+/*
+Load Peers are used for the client to know the ip and ports, of the other clients.
+The function needs to know the clients own ID, which is given when it is initialised in the console,
+as well as the amount of total clients.
+*/
 func loadPeers(selfID, num int) (selfAddr string, peers []string) {
 	selfAddr = fmt.Sprintf("localhost:%d", basePort+selfID)
 	for i := 0; i < num; i++ {
@@ -137,23 +189,30 @@ func loadPeers(selfID, num int) (selfAddr string, peers []string) {
 }
 
 func main() {
+	// Flags are used for giving information to the clients upon initialisation in the console.
+	// To inisialise you write the following: go run . -id=<id> -n=<peer count>
 	var (
 		flagID = flag.Int("id", 0, "this node id (0..n-1)")
 		flagN  = flag.Int("n", 1, "number of nodes")
 	)
-	flag.Parse()
 
+	flag.Parse() // Parse command line flag, and assign
+
+	// Checks for bad console inputs
 	if *flagN <= 0 || *flagID < 0 || *flagID >= *flagN {
 		log.Fatalf("bad flags: id=%d n=%d", *flagID, *flagN)
 	}
 
+	// Loads list of other clients and assigns global variables
 	id = *flagID
 	selfAddr, p := loadPeers(id, *flagN)
 	peers = p
 
+	// Create own server
 	grpcServer := grpc.NewServer()
 	pb.RegisterRicartAgrawalaServer(grpcServer, &RicartAgrawalaServer{})
 
+	// Initialise listener and check error
 	lis, err := net.Listen("tcp", selfAddr)
 	if err != nil {
 		log.Fatalf("listen %s: %v", selfAddr, err)
@@ -161,20 +220,22 @@ func main() {
 
 	log.Printf("[node %d] listening on %s; peers=%v", id, selfAddr, peers)
 
+	// Listen on own port for incomming information
 	go func() {
 		if err := grpcServer.Serve(lis); err != nil {
 			log.Fatalf("grpc serve: %v", err)
 		}
 	}()
 
+	// Endless loop that simulates random critical section requests
 	rand.Seed(time.Now().UnixNano())
 	for {
-		time.Sleep(time.Duration(400+rand.Intn(600)) * time.Millisecond)
+		time.Sleep(time.Duration(400+rand.Intn(500)) * time.Millisecond) // Sleep for random time
 
-		Enter()
+		Enter() // Try to enter critical section
 
-		time.Sleep(time.Duration(300+rand.Intn(500)) * time.Millisecond)
+		time.Sleep(time.Duration(300+rand.Intn(500)) * time.Millisecond) // Critical section for some time
 
-		Exit()
+		Exit() // Leave critical section after some time
 	}
 }
